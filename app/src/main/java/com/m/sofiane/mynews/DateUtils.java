b950849cd6b89e1f3e3df8d404de866ace9773d4
rkoper <sofianem75018@gmail.com>
2019-10-25 10:02:40 +0200

    Final Test 5

--- app/src/main/java/com/m/sofiane/mynews/DateUtils.java
+++ app/src/main/java/com/m/sofiane/mynews/DateUtils.java
@@ -1,29 +1,20 @@
 package com.m.sofiane.mynews;
 
-import android.app.DatePickerDialog;
+import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-
-import butterknife.BindView;
 
 /**
  * created by Sofiane M. 2019-10-09
  */
-@RequiresApi(api = Build.VERSION_CODES.O)
+
+
+@TargetApi(Build.VERSION_CODES.O)
 public class DateUtils {
 
     private final static DateTimeFormatter searchedDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssX");
@@ -31,16 +22,19 @@ public class DateUtils {
     private final static DateTimeFormatter normalDateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yy");
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public static String parseSearchedDate(String date) {
         LocalDateTime dateTime = LocalDateTime.parse(date, searchedDateTimeFormatter);
         return dateTime.format(normalDateTimeFormatter);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public static String parseNormaldDate(String date) {
         OffsetDateTime offsetDateTime = OffsetDateTime.parse(date);
         return offsetDateTime.format(normalDateTimeFormatter);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public static String parseMostPopularDate(String date) {
         LocalDate dateTime = LocalDate.parse(date, mostpopularDateTimeFormatter);
         return dateTime.format(normalDateTimeFormatter);
